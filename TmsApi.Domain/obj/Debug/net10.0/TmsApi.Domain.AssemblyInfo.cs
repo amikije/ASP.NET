8a16c8326d4c828f833bed67bdfcab85dacf15e8
Amir Jelalu <105013135+amikije@users.noreply.github.com>
2026-07-27 21:29:40 +0300

    Add Domain and Infrastructure projects

--- TmsApi.Domain/obj/Debug/net10.0/TmsApi.Domain.AssemblyInfo.cs
+++ TmsApi.Domain/obj/Debug/net10.0/TmsApi.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1ca38a8245602109f6211b3c077d828c4fc7b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3455880bb6f10fe9cd124c8b235faa2eb5fa0205")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
