3455880bb6f10fe9cd124c8b235faa2eb5fa0205
Amir Jelalu <105013135+amikije@users.noreply.github.com>
2026-07-27 21:07:44 +0300

    g

--- TmsApi.Infrastructure/obj/Debug/net10.0/TmsApi.Infrastructure.AssemblyInfo.cs
+++ TmsApi.Infrastructure/obj/Debug/net10.0/TmsApi.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1ca38a8245602109f6211b3c077d828c4fc7b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
